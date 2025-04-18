ae2e674345aeae09837decd6e1cd74bfd6515779
CursedUEFI <138137862+obseletecode72@users.noreply.github.com>
2025-04-18 00:56:54 -0300

    Update main.cpp

--- Server/ImGui Standalone/main.cpp
+++ Server/ImGui Standalone/main.cpp
@@ -1,20 +1,14 @@
 #include "UI.hpp"
 
-void AlocarConsole() {
-    // Aloca o console
+void AlocarConsole() 
+{
     AllocConsole();
-
-    // Redireciona std::cout para o console
     FILE* fp_out;
     freopen_s(&fp_out, "CONOUT$", "w", stdout);
     std::cout.clear();
-
-    // Redireciona std::cin para o console
     FILE* fp_in;
     freopen_s(&fp_in, "CONIN$", "r", stdin);
     std::cin.clear();
-
-    // (Opcional) Redireciona std::cerr e std::clog também
     FILE* fp_err;
     freopen_s(&fp_err, "CONOUT$", "w", stderr);
     std::cerr.clear();
